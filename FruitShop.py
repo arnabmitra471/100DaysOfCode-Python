8d80dcade626ea99cbcea0644353b92c942a7438
Arnab <arnabmitra471@gmail.com>
2023-09-08 22:08:58 +0530

    Wrote logic to buy fruits

--- FruitShop.py
+++ FruitShop.py
@@ -1,3 +1,7 @@
-applePrice = 60
+applePrice = 600
 budget = int(input("Enter your budget: "))
 
+if applePrice <= budget:
+    print("Alexa add apples to my cart")
+else:
+    print("Alexa don't add apples to my cart")
