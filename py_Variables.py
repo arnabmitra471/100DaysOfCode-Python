147ebf02ba773290266b1e6b762cbf960333fdbf
Arnab <arnabmitra471@gmail.com>
2023-09-09 22:41:21 +0530

    Day 6 completed

--- py_Variables.py
+++ py_Variables.py
@@ -1,3 +1,9 @@
+'''
+Numeric datatypes: int,float and complex
+sequence data types: str,list,tuple
+mutable - Updation at the same address is possible
+immutable - updation at the same address is not possible
+'''
 a = 167.46
 print(a)
 a1 = 100
@@ -9,6 +15,8 @@ print(c)
 d = None
 print(d)
 z1 = complex(8,2)
+l1 = ["Arnab","Shubham","Vrinda","Gunraaj",98,23,45,67,12,90,80,45.98,12.96,34,78]
+t1 = (65,87,90,23,87,30,12,109,198,176,154)
 print(a+a1)
 print("The type of a is",type(a))
 print("The type of b is",type(b))
@@ -16,3 +24,10 @@ print("The type of c is",type(c))
 print("The type of d is",type(d))
 print("The type of z1 is",type(z1))
 print(z1)
+print("The type of l1 is",type(l1))
+print("The type of t1 is",type(t1))
+print(l1)
+print(t1)
+d1 = {1:"Aakash",2:"Arnab",3:"Akash Mandal",4:"Arkaprava",5:"Arnab"}
+print(d1)
+print("The type of d1 is",type(d1))
