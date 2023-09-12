ece608986e8b358ab2b5ce56cd075ea08b30bce6
Arnab <arnabmitra471@gmail.com>
2023-09-12 20:50:36 +0530

    Demonstrating and explaining logical operators in python

--- Operators.py
+++ Operators.py
@@ -1,22 +1,28 @@
+# arithmetic operatoes
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
 sum1 = num1 + num2
 diff = num1 - num2
 prod = num1 * num2
-quot = num1/num2
-rem = num1%num2
+# quot = num1/num2
+# rem = num1%num2
 exp = num1 ** num2
-floorQuot = num1 // num2
+# floorQuot = num1 // num2
 
 print("The sum of the two numbers is: ",sum1)
 print("The difference between the two numbers is",diff)
 print("The product of the numbers is",prod)
-print("The quotient of the numbers is",quot)
-print("The remainder division is",rem)
-
+# print("The quotient of the numbers is",quot)
+# print("The remainder division is",rem)
+# Relational operators
 print("num1 > num2=",num1>num2)
 print("num1 < num2=",num1 <num2)
 print("num1 >= num2=",num1>=num2)
 print("num1 <= num2=",num1<=num2)
 print("num1 == num2 =",num1==num2)
 print("num1 != num2 =",num1!=num2)
+
+# Logical Operators
+print("num1 and num2 = ",num1 and num2)
+print("num1 or num2 = ",num1 or num2)
+print("not(num2) = ", not(num2))
