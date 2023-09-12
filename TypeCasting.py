3eaed225e454d31898e9817228ba100baf6ab2e0
Arnab <arnabmitra471@gmail.com>
2023-09-12 21:50:08 +0530

    Describing things about type casting in python

--- TypeCasting.py
+++ TypeCasting.py
@@ -1,3 +1,11 @@
+"""
+There are two types of type casting in python:
+1. Implicit type casting
+2. Explicit type casting
+
+Implicit type casting - Done by the python interpreter itself
+Explicit type casting - Done explicity by the programmer
+"""
 c = "45"
 d = "34.87"
 res = int(c) + float(d)
