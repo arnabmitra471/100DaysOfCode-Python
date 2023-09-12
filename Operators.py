f06eaa6386d715159abc6937dd3f69ad7699418b
Arnab <arnabmitra471@gmail.com>
2023-09-12 19:29:23 +0530

    Operators in python

---
+++ Operators.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+sum1 = num1 + num2
+diff = num1 - num2
+prod = num1 * num2
+quot = num1/num2
+rem = num1%num2
+exp = num1 ** num2
+floorQuot = num1 // num2
+
+print("The sum of the two numbers is: ",sum1)
+print("The difference between the two numbers is",diff)
+print("The product of the numbers is",prod)
+print("The quotient of the numbers is",quot)
+print("The remainder division is",rem)
