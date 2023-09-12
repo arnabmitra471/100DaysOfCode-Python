8678eb731a5e04cb9ea559fa58f42c823a5bcc13
Arnab <arnabmitra471@gmail.com>
2023-09-12 22:13:59 +0530

    Taking user input in python

---
+++ UserInput.py
@@ -0,0 +1,10 @@
+'''
+n = input()
+print(n)
+'''
+x = int(input("Enter first number: "))
+print(type(x))
+y = int(input("Enter second number: "))
+print(type(y))
+res1 = x + y
+print("The sum is",res1)
